8a794dbde9ff27049575d865e2d78e71118caf15
Pedro Pondian <overdadeiropedrojoao53@gmail.com>
2025-11-18 19:33:50 -0300

    Add files via upload

--- Aula13_Vetores2/Exercicio12.java
+++ Aula13_Vetores2/Exercicio12.java
@@ -3,31 +3,28 @@ import java.util.Scanner;
 public class Exercicio12 {
 
 	public static void main(String[] args) {
-		// 45 27 75 32 84 5 64 66 62 46
-		// testar se a senha existe, e se ela ja foi usada
+		//testar se a senha existe, e se ela ja foi usada
+		//         45 27 75 32 84 10 64 66 62 46
+		//usadas = 45 -1 -1 -1 -1 -1 -1 -1 -1 -1
 		Scanner sc = new Scanner(System.in);
-		int a[], u[], nUsadas=0, i, j;
+		int a[], u[], nUsadas=0,i,j=0;
 		final int TAM = 10;
 		a = new int[TAM];
 		u = new int[TAM];
-		boolean ex=false;
 		
 		for(i=0;i<TAM;i++) {
 			a[i]=(int)(Math.random()*100);
+			u[i]=-1;
 		}
 		
-		while(true) {
+		while(j<2) {
+			boolean ex=false;
+			
 			for(i=0;i<TAM;i++) {
 				System.out.print(a[i]+" ");
 			}
 			System.out.println();
-			
-			for(i=0;i<TAM;i++) {
-				System.out.print(u[i]+" ");
-			}
-			System.out.println();
-			
-			
+
 			int s=sc.nextInt();
 			
 			for(i=0;i<TAM;i++) {
@@ -42,7 +39,7 @@ public class Exercicio12 {
 					if(u[i]==s) {
 						System.out.println("Nao vai repetir");
 						break;
-					}else if(u[i]!=s) {
+					}else if(u[i]!=s && i==TAM-1) {
 						System.out.println("Vai la comer amigao");
 						u[nUsadas]=s;
 						nUsadas++;
@@ -52,6 +49,7 @@ public class Exercicio12 {
 			}else {
 				System.out.println("A senha nao existe");
 			}
+			j++;
 		}
 		
 	}
